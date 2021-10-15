ecb57ee3ff5daf356f02dff0cfe952ae665efaba
Adrian A Gonzalez <agonzalez.aveo@gmail.com>
2021-10-15 13:05:30 -0400

    updates

---
+++ ANSDProject/app/src/main/java/com/locobird00/ansdproject/ui/main/MainFragment.kt
@@ -0,0 +1,51 @@
+package com.locobird00.ansdproject.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.locobird00.ansdproject.R
+import com.locobird00.ansdproject.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        if (viewModel.getNames() != "")
+            binding.namesView.text = viewModel.getNames()
+
+        binding.addName.setOnClickListener {
+            if (binding.nameInput.text.isNotEmpty()) {
+                viewModel.setName(binding.nameInput.text.toString())
+                binding.namesView.text = viewModel.getNames()
+            }
+        }
+    }
+}
